2b64526ecd14eb3c5e5b55d292b4312c9abc3035
Matthew Laird <lairdm@ebi.ac.uk>
2018-07-25 16:21:31 +0100

    Typo

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
 
     entry_points = {
         'console_scripts': ['sam_alignment_reconstructor=sam_alignment_reconstructor.cli:main'],
-        }
+        },
       
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
